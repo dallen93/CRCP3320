fcae7698e57267bdc3818fcfc4c4e4fbdc76da40
D'Marquis Allen <dmarquisallen@gmail.com>
2015-09-02 01:17:43 -0500

    changed file extension for index.html to index.php. Also created buttonmenu.php to hold the menu for all of the buttons to be clicked on each page.

--- project01/index.php
+++ project01/index.php
@@ -20,12 +20,7 @@
 <body>
   <h1>Top 100 Albums of All Time</h1>
   <h2>At least, according to someone.</h2>
-  <form>
-    <input type="button" value="Home" onclick="location.href='index.html'" />
-    <input type="button" value="Rank" onClick="location.href='rank.php'" />
-    <input type="button" value="Title" onclick="location.href='title.php'" />
-    <input type="button" value="Year" onclick="location.href='year.php'" />
-  </form>
+  <?php include 
 <pre>
 Sgt. Pepper's Lonely Hearts Club Band, 1967
 Are You Experienced?, 1967
