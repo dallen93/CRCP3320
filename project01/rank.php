afd58bcb03e15400056458e86803d03a02a4e4b1
D'Marquis Allen <dmarquisallen@gmail.com>
2015-09-03 20:52:37 -0500

    added header.php to rank.php

--- project01/rank.php
+++ project01/rank.php
@@ -1,27 +1,4 @@
-<html>
-<head>
-  <title>Top 100 Albums of All Time</title>
-  <meta name="description" content="A list of the top 100 
-  albums of All Time, according to Rolling Stone.">
-  <style>
-    h1 {
-      font-family: helvetica;
-      font-weight: normal;
-      color: #CC98FF;
-    }
-    h2 {
-      font-family: sans-serif;
-      font-weight: italic;
-      color: #EE98FF;
-    }
-  </style>
-</head>
-
-<body>
-  <h1>Top 100 Albums of All Time</h1>
-  <h2>At least, according to someone.</h2>
-
-<?php include ("buttonmenu.php"); ?>
+<? include ("header.php") ?>
  
 <table>
   <tr>
