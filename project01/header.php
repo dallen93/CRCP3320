b326875888ef44bd1c095711e7a25283af036e6f
D'Marquis Allen <dmarquisallen@gmail.com>
2015-09-05 17:25:04 -0500

    removed redundant code from title.php and successfully added include statements for header.php and footer.php

--- project01/header.php
+++ project01/header.php
@@ -26,3 +26,21 @@
     <input type="button" value="Title" onclick="location.href='title.php'" />
     <input type="button" value="Year" onclick="location.href='year.php'" />
   </form>
+  
+<table>
+  <thead>
+    <tr>
+      <td>Rank</td>
+      <td>Title</td>
+      <td>Year</td>
+    </tr>
+  </thead>
+<?php
+$servername = "crcp3320db.humanoriented.com";
+$username = "crcp3320";
+$password = "Crcp3320";
+$dbname = "crcp3320";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
--- project01/title.php
+++ project01/title.php
@@ -1,29 +1,7 @@
-<?php include ("header.php"); ?>
- 
-<table>
-  <thead>
-    <tr>
-      <td>Rank</td>
-      <td>Title</td>
-      <td>Year</td>
-    </tr>
-  </thead>
-<?php
-$servername = "crcp3320db.humanoriented.com";
-$username = "crcp3320";
-$password = "Crcp3320";
-$dbname = "crcp3320";
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-$sql = "SELECT * FROM albums ORDER BY title;";
-$result = $conn->query($sql);
-while($row = $result->fetch_assoc()) {
-    echo "<tr>\n" . "\t<td>" . $row["rank"] . "</td>\n<td>" . $row["title"]. "</td>\n<td>" . $row["year"]. "</td>\n</tr>\n";
-}
-$conn->close();
-?>
+<?php include ("header.php");
+  
+$sql = "SELECT * FROM albums ORDER BY rank;";
 
-</table>
-<? include ("footer.php") ?>
+ include ("footer.php") 
+
+ ?>
